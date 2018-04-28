1603e13eaaf71a3c85405db59a01726f5e20c8ed
RyanLPrince <37381046+RyanLPrince@users.noreply.github.com>
2018-04-28 22:34:35 +0100

    js won't send request to localhost

--- DashBoard.js
+++ DashBoard.js
@@ -17,16 +17,39 @@ class Account{
     }
     
 }
+ 
 
 function createAccount(){
-    //window.alert("Hello");
     var account=new Account(document.getElementById("firstName").value,
                         document.getElementById("surname").value,
                         document.getElementById("accountNumber").value)
     
     var accountAsJSON=JSON.stringify(account)
     window.alert(accountAsJSON);
+    sendRequest(accountAsJSON,account.getAccountNumber());
    
 }
 
+function sendRequest(accountAsJSON){
+    var xhr=new XMLHttpRequest();
+    xhr.open('POST',"http://localhost:8080/Application-Management-App/api/account/json/",true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(accountAsJSON);
+    
+    xhr.addEventListener("readystatechange", processRequest, false);
+    xhr.onreadystatechange = processRequest;
+ 
+    
+   
+   
+}
 
+ function processRequest(e) {   
+     window.alert("invoked");
+     window.alert(xhr.status+"");
+     if (xhr.readyState == 4 && xhr.status == 200) {
+       //var response = JSON.parse(xhr.responseText);//redundnat?
+        window.alert(responseText);
+    }
+        
+    }
